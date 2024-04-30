526b66af7456b2c38ae348b58cf2935d0eb2a592
taha <taha68496@gmail.com>
2024-04-29 21:53:08 -0700

    first

---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from 'inquirer';
+let todolist = [];
+let condition = true;
+
+while(condition)
+{
+  let Task = await inquirer.prompt(
+[
+    {
+      name: 'todolist',
+      type: 'input',
+      message: "add items in list"
+
+    },
+    {
+      name: 'add',
+      type: 'confirm',
+      message: 'add more ',
+      default: 'false'
+    }
+  ]
+  );
+  todolist.push(Task.todolist);
+  condition = Task.add
+  console.log(todolist)
+}
